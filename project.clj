dad2cbab6986800b57f3a4f7e9798b3e7c1e71b3
Muazzam Ali <muazzam_ali@live.com>
2023-09-01 20:48:05 +0500

    downgraded azure lib version for compatibility

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [com.azure/azure-data-appconfiguration "1.4.8"]
+                 [com.azure/azure-data-appconfiguration "1.3.0"]
                  [com.amazonaws/aws-java-sdk-secretsmanager "1.12.540"]]
   :main ^:skip-aot fractl-config-secrets-reader.core
   :target-path "target/%s"
