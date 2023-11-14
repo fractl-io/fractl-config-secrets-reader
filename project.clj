170eee59e7731ce591b74f11fbf0b568891754fe
Vijay Mathew <vijay@fractl.io>
2023-11-14 14:25:56 +0530

    project prefix change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fractl-io/fractl-config-secrets-reader "0.1.0"
+(defproject com.github.fractl-io/fractl-config-secrets-reader "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
